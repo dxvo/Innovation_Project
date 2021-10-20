2ed8fab51d559aa39f38b29662dcdabc110bac89
dxvo <dxvo@uh.edu>
2021-10-19 20:19:27 -0500

    update

---
+++ app.py
@@ -0,0 +1,19 @@
+from flask import Flask, render_template, url_for, flash, redirect, request,jsonify
+
+
+app = Flask(__name__)
+
+
+@app.route("/",methods=['GET', 'POST'])
+@app.route("/home",methods=['GET', 'POST'])
+def home():
+    return render_template('index.html')
+
+
+if __name__ == '__main__':
+    app.run(debug=True)
+
+
+
+
+
