06c79980825e711c6ad7d58f766e73415b7a58a4
dxvo <dxvo@uh.edu>
2021-10-20 23:52:03 -0500

    update template

--- app.py
+++ app.py
@@ -1,4 +1,5 @@
-from flask import Flask, render_template, url_for, flash, redirect, request,jsonify
+from flask import Flask, render_template, url_for, redirect, request,jsonify
+import os
 app = Flask(__name__)
 
 
@@ -9,7 +10,12 @@ def home():
 
 @app.route("/upload",methods=['GET', 'POST'])
 def upload():
-    return render_template('Upload.html')
+    if request.method == "POST":
+        file = request.files["file"]
+        file.save(os.path.join("uploads",file.filename))
+        #return redirect(url_for('home'))
+        return render_template('upload.html',message="File has been successfully uploaded")
+    return render_template('upload.html',message="")
 
 if __name__ == '__main__':
     app.run(debug=True)
