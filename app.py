09b0d51001862b4b07fcbc7d075003c081607453
dxvo <dxvo@uh.edu>
2021-10-21 01:00:26 -0500

    update

--- app.py
+++ app.py
@@ -1,7 +1,8 @@
 from flask import Flask, render_template, url_for, redirect, request,jsonify
 import os
 app = Flask(__name__)
-app.config["UPLOADS"] ="/Users/devo/Desktop/Innovation_Project/uploads"
+
+#app.config["UPLOADS"] ="/Users/devo/Desktop/Innovation_Project/uploads"
 
 
 @app.route("/",methods=['GET', 'POST'])
@@ -14,8 +15,8 @@ def home():
 def upload():
     if request.method == "POST":
         file = request.files["file"]
-        #file.save(os.path.join(app.config["UPLOADS"],file.filename))
-        file.save(os.path.join( os.path.dirname(__file__),app.config["UPLOADS"],file.filename))
+        file.save(os.path.join("/app/uploads",file.filename))
+        #file.save(os.path.join( os.path.dirname(__file__),app.config["UPLOADS"],file.filename))
         #return redirect(url_for('home'))
         return render_template('upload.html',message="File has been successfully uploaded")
     return render_template('upload.html',message="")
